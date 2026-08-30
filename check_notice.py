30ea4c473142247377ec6497dc33fd389582092e
sungkwangcho <80678217+sungkwangcho@users.noreply.github.com>
2026-08-30 23:26:20 +0900

    Update check_notice.py
    py code start

--- check_notice.py
+++ check_notice.py
@@ -1,1 +1,15 @@
-print("제우스 공지 확인 프로그램 실행 성공")
+import os
+import requests
+
+webhook_url = os.environ["DISCORD_WEBHOOK_URL"]
+
+message = {
+    "content": "🔔 제우스 공지 봇 테스트 알림입니다."
+}
+
+response = requests.post(
+    webhook_url,
+    json=message
+)
+
+print("Discord 응답 코드:", response.status_code)
